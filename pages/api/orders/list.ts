5ec502cb11329a865a9de518d123280ebd537ef8
waleeakhter <waleeakhter@gmail.com>
2022-12-11 17:27:58 +0000

    googlesheet

--- pages/api/orders/list.ts
+++ pages/api/orders/list.ts
@@ -22,11 +22,18 @@ export default async function handler(
         case 'GET':
             const orders = await Order.find({}).populate([{ path: 'product_id', model: Product }, { path: 'shop_id', model: Shop }])
             const refine = orders.map(order => {
-                return { Shop: order.shop_id.shop_name, Product: order.product_id.product_name, Quantity: order.sell_quantity, Price: order.total_amount, }
+                return {
+                    shop: order.shop_id.shop_name,
+                    product: order.product_id.product_name,
+                    quantity: order.sell_quantity,
+                    price: order.sell_price,
+                    total_Price: order.total_amount,
+
+                }
             })
             const sortOrders = refine.sort((a, b) => {
-                if (a.Shop > b.Shop) return 1;
-                if (a.Shop < b.Shop) return -1;
+                if (a.shop > b.shop) return 1;
+                if (a.shop < b.shop) return -1;
                 return 0;
             });
             res.status(200).json(googlesheet === "true" ? sortOrders : orders)
